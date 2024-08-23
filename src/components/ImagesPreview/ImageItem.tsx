fa6cb0ff9a2e29e5772b23c5a68f9be7f6267c05
TahirReyaz <tahirreyaz5@gmail.com>
2024-08-23 14:04:24 +0530

    UI fix

--- src/components/ImagesPreview/ImageItem.tsx
+++ src/components/ImagesPreview/ImageItem.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaDownload } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 
 const ImageItem = ({
   removeImage,
@@ -17,24 +19,28 @@ const ImageItem = ({
   };
 
   return (
-    <div key={index} className="flex flex-col items-center">
+    <div key={index} className="flex flex-col items-center relative">
       <img
         src={src}
         alt={`Captured ${index + 1}`}
-        className="border rounded mb-2 w-40 h-40 object-contain"
+        className="rounded mb-2 w-40 h-40 object-contain"
       />
-      <button
-        onClick={() => downloadImage(src)}
-        className="bg-purple-500 text-white p-2 rounded mb-2"
-      >
-        Download
-      </button>
-      <button
-        onClick={() => removeImage(index)}
-        className="bg-red-500 text-white p-2 rounded"
-      >
-        Remove
-      </button>
+      <div className="absolute z-10 p-2 right-0 h-full flex flex-col justify-between items-end w-fit">
+        <div className="cursor-pointer text-anilist-aqua_haze bg-anilist-mandy p-1 rounded">
+          <RxCross2
+            {...{
+              onClick: () => removeImage(index),
+            }}
+          />
+        </div>
+        <div className="cursor-pointer text-anilist-aqua_haze bg-anilist-cerulean p-1 rounded">
+          <FaDownload
+            {...{
+              onClick: () => downloadImage(src),
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ImagesPreview/index.tsx
+++ src/components/ImagesPreview/index.tsx
@@ -10,10 +10,10 @@ const ImagesPreview = ({
 }) => {
   return (
     <div className="flex flex-col items-center bg-anilist-mirage p-4">
-      <h2 className="text-xl font-bold mb-4">
-        Captured Images (will fix the UI)
+      <h2 className="text-xl font-medium mb-4 text-anilist-aqua_haze">
+        Captured Images
       </h2>
-      {capturedImages.length > 0 && (
+      {capturedImages.length > 0 ? (
         <div className="grid grid-cols-2 gap-4">
           {capturedImages.map((src: string, index: number) => (
             <ImageItem
@@ -25,6 +25,8 @@ const ImagesPreview = ({
             />
           ))}
         </div>
+      ) : (
+        <h3 className="text-lg text-anilist-aqua_haze">No images</h3>
       )}
     </div>
   );
